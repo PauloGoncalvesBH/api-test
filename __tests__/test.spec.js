08108a8e9eb76daacc6a9750586859b294c2a75a
Paulo Gonçalves <paulorochag@hotmail.com>
2020-03-18 08:20:16 -0300

    style(teste.spec.js): padronização do código com 'standard'

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -1,18 +1,18 @@
-const faker = require("faker");
-const frisby = require("frisby");
-const Joi = frisby.Joi;
+const faker = require('faker')
+const frisby = require('frisby')
+const Joi = frisby.Joi
 
-describe("Requisições utilizando token", () => {
-  const porta = 3000;
+describe('Requisições utilizando token', () => {
+  const porta = 3000
 
   // Autenticar e adicionar o token no header de todas as requisições
   beforeAll(() => {
     return frisby
       .post(`http://localhost:${porta}/auth/login`, {
-        email: "paulo@email.com",
-        password: "paulo"
+        email: 'paulo@email.com',
+        password: 'paulo'
       })
-      .expect("status", 200)
+      .expect('status', 200)
       .then(response => {
         frisby.globalSetup({
           request: {
@@ -20,48 +20,48 @@ describe("Requisições utilizando token", () => {
               Authorization: `Bearer ${response.json.token}`
             }
           }
-        });
-      });
-  });
+        })
+      })
+  })
 
-  it("GET aluno 2, validar status e id", () => {
-    const idAluno = 2;
+  it('GET aluno 2, validar status e id', () => {
+    const idAluno = 2
     return frisby
       .get(`http://localhost:${porta}/alunos/${idAluno}`)
-      .expect("status", 200)
-      .expect("json", {
+      .expect('status', 200)
+      .expect('json', {
         id: idAluno
-      });
-  });
+      })
+  })
 
-  it("GET turma 1, validar status e contrato", () => {
+  it('GET turma 1, validar status e contrato', () => {
     return frisby
       .get(`http://localhost:${porta}/turmas/1`)
-      .expect("status", 200)
-      .expect("jsonTypes", {
+      .expect('status', 200)
+      .expect('jsonTypes', {
         id: Joi.number(),
         descricao: Joi.string(),
         idHorario: Joi.number(),
         idAluno: Joi.number()
-      });
-  });
+      })
+  })
 
-  it("GET turma 2 GET horário, validar status e id [Requisições aninhadas]", () => {
+  it('GET turma 2 GET horário, validar status e id [Requisições aninhadas]', () => {
     return frisby
       .get(`http://localhost:${porta}/turmas/2`)
-      .expect("status", 200)
+      .expect('status', 200)
       .then((res) => {
-        let idHorario = res.json.idHorario;
+        const idHorario = res.json.idHorario
         return frisby
           .get(`http://localhost:${porta}/horarios/${idHorario}`)
-          .expect("status", 200)
-          .expect("json", {
+          .expect('status', 200)
+          .expect('json', {
             id: idHorario
-          });
-      });
-  });
+          })
+      })
+  })
 
-  it("POST auth/registrar POST auth/login, validar endpoints de autenticação [Requisições aninhadas]", () => {
+  it('POST auth/registrar POST auth/login, validar endpoints de autenticação [Requisições aninhadas]', () => {
     const newUserEmail = faker.internet.email()
     const newUserPassword = faker.internet.password()
     json = {
@@ -70,11 +70,11 @@ describe("Requisições utilizando token", () => {
     }
     return frisby
       .post(`http://localhost:${porta}/auth/registrar`, json)
-      .expect("status", 201)
+      .expect('status', 201)
       .then(() => {
         return frisby
           .post(`http://localhost:${porta}/auth/login`, json)
-          .expect("status", 200);
-      });
-  });
-});
+          .expect('status', 200)
+      })
+  })
+})
